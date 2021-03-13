e9412e6dd5864edb0b7d1bf8f34d20c78872bcdd
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-03-14 01:22:47 +0900

    login signup page

---
+++ lib/screens/signinOrSignUp/signin_or_signup_screen.dart
@@ -0,0 +1,39 @@
+import 'package:chat/components/primary_button.dart';
+import 'package:chat/constants.dart';
+import 'package:flutter/material.dart';
+
+class SigninOrSignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Column(
+            children: [
+              Spacer(flex: 2),
+              Image.asset(
+                MediaQuery.of(context).platformBrightness == Brightness.light
+                    ? 'assets/images/Logo_light.png'
+                    : 'assets/images/Logo_dark.png',
+                height: 150,
+              ),
+              Spacer(),
+              PrimaryButton(
+                text: 'Sign In',
+                press: () {},
+              ),
+              SizedBox(height: kDefaultPadding * 1.5),
+              PrimaryButton(
+                text: 'Sign Up',
+                color: Theme.of(context).colorScheme.secondary,
+                press: () {},
+              ),
+              Spacer(flex: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
